971f463fd731c5edec70667514e351975743149a
Italo Pais <Italo Pais@DESKTOP-2LBGGTA>
2023-08-21 13:30:47 -0300

    Adicionar arquivos de projeto.

---
+++ WEBWVC/Models/Aluno.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WEBWVC.Models
+{
+    [Table("Alunos")]
+    public class Aluno
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        [StringLength(35)]
+        public string nome { get; set; }
+
+        [StringLength(30)]
+        public string disciplina { get; set; }
+        public float nota1 { get; set; }
+        public float nota2 { get; set; }
+    }
+}
---
+++ WEBWVC/Models/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WEBWVC.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+}
